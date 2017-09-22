cf8e11aa46f9c45c671a1b3806f075202dfec677
Zhukov Maxim <my.fsou1@gmail.com>
2017-09-22 16:38:03 +0300

    Add OutOfRange guard

---
+++ src/GuardClauses.UnitTests/GuardAgainsOutOfRange.cs
@@ -0,0 +1,33 @@
+﻿using Ardalis.GuardClauses;
+using System;
+using Xunit;
+
+namespace GuardClauses.UnitTests
+{
+    public class GuardAgainsOutOfRange
+    {
+        [Fact]
+        public void DoesNothingGivenInRangeValueUsingShortcutMethod()
+        {
+            Guard.AgainsOutOfRange(2, "index", 1, 5);
+        }
+
+        [Fact]
+        public void DoesNothingGivenInRangeValueUsingExtensionMethod()
+        {
+            Guard.Against.OutOfRange(2, "index", 1, 5);
+        }
+
+        [Fact]
+        public void ThrowsGivenOutOfRangeValueUsingShortcutMethod()
+        {
+            Assert.Throws<ArgumentOutOfRangeException>(() => Guard.AgainsOutOfRange(5, "index", 1, 4));
+        }
+
+        [Fact]
+        public void ThrowsGivenOutOfRangeValueUsingExtensionMethod()
+        {
+            Assert.Throws<ArgumentOutOfRangeException>(() => Guard.Against.OutOfRange(5, "index", 1, 4));
+        }
+    }
+}
