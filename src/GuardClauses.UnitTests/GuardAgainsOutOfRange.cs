3fbcf4eef92fd5fcc007dc4da4be7ed20b0a165d
Zhukov Maxim <my.fsou1@gmail.com>
2017-09-22 17:38:06 +0300

    Improve inline data for existing usecases

--- src/GuardClauses.UnitTests/GuardAgainsOutOfRange.cs
+++ src/GuardClauses.UnitTests/GuardAgainsOutOfRange.cs
@@ -7,36 +7,36 @@ namespace GuardClauses.UnitTests
     public class GuardAgainsOutOfRange
     {
         [Theory]
-        [InlineData(1, 1, 5)]
-        [InlineData(2, 1, 5)]
-        [InlineData(3, 1, 5)]
+        [InlineData(1, 1, 3)]
+        [InlineData(2, 1, 3)]
+        [InlineData(3, 1, 3)]
         public void DoesNothingGivenInRangeValueUsingShortcutMethod(int input, int rangeFrom, int rangeTo)
         {
             Guard.AgainsOutOfRange(input, "index", rangeFrom, rangeTo);
         }
 
         [Theory]
-        [InlineData(1, 1, 5)]
-        [InlineData(2, 1, 5)]
-        [InlineData(3, 1, 5)]
+        [InlineData(1, 1, 3)]
+        [InlineData(2, 1, 3)]
+        [InlineData(3, 1, 3)]
         public void DoesNothingGivenInRangeValueUsingExtensionMethod(int input, int rangeFrom, int rangeTo)
         {
             Guard.Against.OutOfRange(input, "index", rangeFrom, rangeTo);
         }
 
         [Theory]
-        [InlineData(-1, 1, 5)]
-        [InlineData(0, 1, 5)]
-        [InlineData(6, 1, 5)]
+        [InlineData(-1, 1, 3)]
+        [InlineData(0, 1, 3)]
+        [InlineData(4, 1, 3)]
         public void ThrowsGivenOutOfRangeValueUsingShortcutMethod(int input, int rangeFrom, int rangeTo)
         {
             Assert.Throws<ArgumentOutOfRangeException>(() => Guard.AgainsOutOfRange(input, "index", rangeFrom, rangeTo));
         }
 
         [Theory]
-        [InlineData(-1, 1, 5)]
-        [InlineData(0, 1, 5)]
-        [InlineData(6, 1, 5)]
+        [InlineData(-1, 1, 3)]
+        [InlineData(0, 1, 3)]
+        [InlineData(4, 1, 3)]
         public void ThrowsGivenOutOfRangeValueUsingExtensionMethod(int input, int rangeFrom, int rangeTo)
         {
             Assert.Throws<ArgumentOutOfRangeException>(() => Guard.Against.OutOfRange(input, "index", rangeFrom, rangeTo));
