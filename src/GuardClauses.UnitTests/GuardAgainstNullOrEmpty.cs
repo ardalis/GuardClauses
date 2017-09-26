e8f02c1ba4cc025938a4340c9f3247b2ea632f86
Steve Smith <steve@kentsmiths.com>
2017-09-26 17:29:08 -0400

    Removing shortcuts from Guard and using Against extensions throughout for consistency. (#10)

--- src/GuardClauses.UnitTests/GuardAgainstNullOrEmpty.cs
+++ src/GuardClauses.UnitTests/GuardAgainstNullOrEmpty.cs
@@ -7,39 +7,20 @@ namespace GuardClauses.UnitTests
     public class GuardAgainstNullOrEmpty
     {
         [Fact]
-        public void DoesNothingGivenNonEmptyStringValueUsingShortcutMethod()
-        {
-            Guard.AgainstNullOrEmpty("a", "string");
-            Guard.AgainstNullOrEmpty("1", "aNumericString");
-        }
-
-        [Fact]
-        public void DoesNothingGivenNonEmptyStringValueUsingExtensionMethod()
+        public void DoesNothingGivenNonEmptyStringValue()
         {
             Guard.Against.NullOrEmpty("a", "string");
             Guard.Against.NullOrEmpty("1", "aNumericString");
         }
 
         [Fact]
-        public void ThrowsGivenNullValueUsingShortcutMethod()
-        {
-            Assert.Throws<ArgumentNullException>(() => Guard.AgainstNullOrEmpty(null, "null"));
-        }
-
-        [Fact]
-        public void ThrowsGivenEmptyStringUsingShortcutMethod()
-        {
-            Assert.Throws<ArgumentException>(() => Guard.AgainstNullOrEmpty("", "emptystring"));
-        }
-
-        [Fact]
-        public void ThrowsGivenNullValueUsingExtensionMethod()
+        public void ThrowsGivenNullValue()
         {
             Assert.Throws<ArgumentNullException>(() => Guard.Against.NullOrEmpty(null, "null"));
         }
 
         [Fact]
-        public void ThrowsGivenEmptyStringUsingExtensionMethod()
+        public void ThrowsGivenEmptyString()
         {
             Assert.Throws<ArgumentException>(() => Guard.Against.NullOrEmpty("", "emptystring"));
         }
--- src/GuardClauses.UnitTests/NamespaceSeparate/ExtendingGuard.cs
+++ src/GuardClauses.UnitTests/NamespaceSeparate/ExtendingGuard.cs
@@ -3,6 +3,9 @@
 // By using the same namespace, the required using for Ardalis.GuardClauses will include custom guards regardless of location.
 namespace Ardalis.GuardClauses
 {
+    /// <summary>
+    /// An example Guard extension method. Throws if input is "foo".
+    /// </summary>
     public static class FooGuard
     {
         public static void Foo(this IGuardClause guardClause, string input, string parameterName)
