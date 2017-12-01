7ab2c123d7ea85865dbe69623862fa17a4d730f7
Scott DePouw <scott@ardalis.com>
2017-12-01 08:40:05 -0500

    Add tests for DateTime range. Removed redundant Int test.

---
+++ src/GuardClauses.UnitTests/GuardAgainstOutOfRangeForDateTime.cs
@@ -0,0 +1,44 @@
+﻿using Ardalis.GuardClauses;
+using System;
+using Xunit;
+
+namespace GuardClauses.UnitTests
+{
+    public class GuardAgainstOutOfRangeForDateTime
+    {
+        [Theory]
+        [InlineData(0, 0)]
+        [InlineData(0, 3)]
+        [InlineData(-1, 1)]
+        [InlineData(-1, 0)]
+        public void DoesNothingGivenInRangeValue(int rangeFromOffset, int rangeToOffset)
+        {
+            DateTime input = DateTime.Now;
+            DateTime rangeFrom = input.AddSeconds(rangeFromOffset);
+            DateTime rangeTo = input.AddSeconds(rangeToOffset);
+            Guard.Against.OutOfRange(input, "index", rangeFrom, rangeTo);
+        }
+
+        [Theory]
+        [InlineData(1, 3)]
+        [InlineData(-4, -3)]
+        public void ThrowsGivenOutOfRangeValue(int rangeFromOffset, int rangeToOffset)
+        {
+            DateTime input = DateTime.Now;
+            DateTime rangeFrom = input.AddSeconds(rangeFromOffset);
+            DateTime rangeTo = input.AddSeconds(rangeToOffset);
+            Assert.Throws<ArgumentOutOfRangeException>(() => Guard.Against.OutOfRange(input, "index", rangeFrom, rangeTo));
+        }
+
+        [Theory]
+        [InlineData(3, 1)]
+        [InlineData(3, -1)]
+        public void ThrowsGivenInvalidArgumentValue(int rangeFromOffset, int rangeToOffset)
+        {
+            DateTime input = DateTime.Now;
+            DateTime rangeFrom = input.AddSeconds(rangeFromOffset);
+            DateTime rangeTo = input.AddSeconds(rangeToOffset);
+            Assert.Throws<ArgumentException>(() => Guard.Against.OutOfRange(DateTime.Now, "index", rangeFrom, rangeTo));
+        }
+    }
+}
