75db59184240d390661cce30ebbdb5b8b3f0c06b
Kyle McMaster <KyleMcMaster@users.noreply.github.com>
2019-04-08 09:39:48 -0400

    Add Guard.Against.Default and tests (#38)

---
+++ src/GuardClauses.UnitTests/GuardAgainstDefault.cs
@@ -0,0 +1,29 @@
+﻿using Ardalis.GuardClauses;
+using System;
+using Xunit;
+
+namespace GuardClauses.UnitTests
+{
+    public class GuardAgainstDefault
+    {
+        [Fact]
+        public void DoesNothingGivenNonDefaultValue()
+        {
+            Guard.Against.Default("", "string");
+            Guard.Against.Default(1, "int");
+            Guard.Against.Default(Guid.NewGuid(), "guid");
+            Guard.Against.Default(DateTime.Now, "datetime");
+            Guard.Against.Default(new Object(), "object");
+        }
+
+        [Fact]
+        public void ThrowsGivenDefaultValue()
+        {
+            Assert.Throws<ArgumentException>(() => Guard.Against.Default(default(string), "string"));
+            Assert.Throws<ArgumentException>(() => Guard.Against.Default(default(int), "int"));
+            Assert.Throws<ArgumentException>(() => Guard.Against.Default(default(Guid), "guid"));
+            Assert.Throws<ArgumentException>(() => Guard.Against.Default(default(DateTime), "datetime"));
+            Assert.Throws<ArgumentException>(() => Guard.Against.Default(default(object), "object"));
+        }
+    }
+}
