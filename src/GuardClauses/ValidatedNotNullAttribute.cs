dcdbe0cadffeb8113aec1802942b373d018ab34b
Steve Smith <steve@kentsmiths.com>
2020-02-13 14:56:03 -0500

    adding support for ValidatedNotNullAttrib (#60)

---
+++ src/GuardClauses/ValidatedNotNullAttribute.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace Ardalis.GuardClauses
+{
+    /// <summary>
+    /// Add to methods that check input for null and throw if the input is null.
+    /// </summary>
+    [AttributeUsage(AttributeTargets.Parameter, AllowMultiple = false, Inherited = false)]
+    public sealed class ValidatedNotNullAttribute : Attribute { }
+}
