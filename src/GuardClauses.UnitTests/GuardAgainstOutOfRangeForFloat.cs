6e4d6da9ac167cdf4285a2715e75923b38d94d8d
Paul Bentley <paul@paulbentley.co.uk>
2020-03-13 16:37:51 +0000

    OutOfRange overloads for double and float (#62)

---
+++ src/GuardClauses.UnitTests/GuardAgainstOutOfRangeForFloat.cs
@@ -0,0 +1,37 @@
+﻿using Ardalis.GuardClauses;
+using System;
+using Xunit;
+
+namespace GuardClauses.UnitTests
+{
+    public class GuardAgainstOutOfRangeForFloat
+    {
+        [Theory]
+        [InlineData(1.0, 1.0, 1.0)]
+        [InlineData(1.0, 1.0, 3.0)]
+        [InlineData(2.0, 1.0, 3.0)]
+        [InlineData(3.0, 1.0, 3.0)]
+        public void DoesNothingGivenInRangeValue(float input, float rangeFrom, float rangeTo)
+        {
+            Guard.Against.OutOfRange(input, "index", rangeFrom, rangeTo);
+        }
+
+        [Theory]
+        [InlineData(-1.0, 1.0, 3.0)]
+        [InlineData(0.0, 1.0, 3.0)]
+        [InlineData(4.0, 1.0, 3.0)]
+        public void ThrowsGivenOutOfRangeValue(float input, float rangeFrom, float rangeTo)
+        {
+            Assert.Throws<ArgumentOutOfRangeException>(() => Guard.Against.OutOfRange(input, "index", rangeFrom, rangeTo));
+        }
+
+        [Theory]
+        [InlineData(-1.0, 3.0, 1.0)]
+        [InlineData(0.0, 3.0, 1.0)]
+        [InlineData(4.0, 3.0, 1.0)]
+        public void ThrowsGivenInvalidArgumentValue(float input, float rangeFrom, float rangeTo)
+        {
+            Assert.Throws<ArgumentException>(() => Guard.Against.OutOfRange(input, "index", rangeFrom, rangeTo));
+        }
+    }
+}
