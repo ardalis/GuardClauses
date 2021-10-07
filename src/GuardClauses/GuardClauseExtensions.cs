9d82b154b5f187852a86eab14d3f82d32d4614bc
Mohaan Balakrishnan <55804662+mohaanwrites@users.noreply.github.com>
2021-10-07 22:39:25 +0530

    Support for custom Guards using expression (#118)
    Co-authored-by: Steve Smith <steve@kentsmiths.com>

--- src/GuardClauses/GuardClauseExtensions.cs
+++ src/GuardClauses/GuardClauseExtensions.cs
@@ -900,5 +900,25 @@ namespace Ardalis.GuardClauses
 
             return input;
         }
+
+        /// <summary>
+        /// Throws an <see cref="ArgumentException" /> if <paramref name="func"/> evaluates to false for given <paramref name="input"/> 
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="func"></param>
+        /// <param name="guardClause"></param>
+        /// <param name="input"></param>
+        /// <param name="message"></param>
+        /// <returns><paramref name="input"/> if the <paramref name="func"/> evaluates to true </returns>
+        /// <exception cref="ArgumentException"></exception>
+        public static T AgainstExpression<T>([JetBrainsNotNull] this IGuardClause guardClause, [JetBrainsNotNull] Func<T, bool> func, T input, string message) where T : struct
+        {
+            if (!func(input))
+            {
+                throw new ArgumentException(message);
+            }
+
+            return input;
+        }
     }
 }
