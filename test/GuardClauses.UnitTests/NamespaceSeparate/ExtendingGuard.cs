c4f7c03596b40cdcb2db74af91debc42a58825a3
Steve Smith <steve@kentsmiths.com>
2022-02-18 10:37:43 -0500

    file-scoped namespace

--- test/GuardClauses.UnitTests/NamespaceSeparate/ExtendingGuard.cs
+++ test/GuardClauses.UnitTests/NamespaceSeparate/ExtendingGuard.cs
@@ -2,21 +2,20 @@
 using System.Runtime.CompilerServices;
 
 // By using the same namespace, the required using for Ardalis.GuardClauses will include custom guards regardless of location.
-namespace Ardalis.GuardClauses
+namespace Ardalis.GuardClauses;
+
+/// <summary>
+/// An example Guard extension method. Throws if input is "foo".
+/// </summary>
+public static class FooGuard
 {
-    /// <summary>
-    /// An example Guard extension method. Throws if input is "foo".
-    /// </summary>
-    public static class FooGuard
-    {
 #if NETSTANDARD || NETFRAMEWORK
         public static void Foo(this IGuardClause guardClause, string input, string parameterName)
 #else
-        public static void Foo(this IGuardClause guardClause, string input, [CallerArgumentExpression("input")] string? parameterName = null)
+    public static void Foo(this IGuardClause guardClause, string input, [CallerArgumentExpression("input")] string? parameterName = null)
 #endif
-        {
-            if (input?.ToLower() == "foo")
-                throw new ArgumentException("Should not have been foo!", parameterName);
-        }
+    {
+        if (input?.ToLower() == "foo")
+            throw new ArgumentException("Should not have been foo!", parameterName);
     }
 }
