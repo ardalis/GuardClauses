52636fedd6b343bfe8063c2c24a3bf974cfc9683
KonH <konh@yandex.ru>
2022-10-10 21:13:27 +0200

    Remove unused usings in new file

--- src/GuardClauses/GuardAgainstEmptyOrWhiteSpaceExtensions.cs
+++ src/GuardClauses/GuardAgainstEmptyOrWhiteSpaceExtensions.cs
@@ -1,8 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Diagnostics.CodeAnalysis;
-using System.Linq;
-using System.Runtime.CompilerServices;
 
 namespace Ardalis.GuardClauses
 {
