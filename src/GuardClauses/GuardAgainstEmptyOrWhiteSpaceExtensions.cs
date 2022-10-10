3f666822c92a41f9624a07addb5a4783be0a13b3
KonH <konh@yandex.ru>
2022-10-10 21:07:17 +0200

    Move new methods in separated file

---
+++ src/GuardClauses/GuardAgainstEmptyOrWhiteSpaceExtensions.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Linq;
+using System.Runtime.CompilerServices;
+
+namespace Ardalis.GuardClauses
+{
+    public static partial class GuardClauseExtensions
+    {
+#if NET5_0_OR_GREATER
+        /// <summary>
+        /// Throws an <see cref="ArgumentException" /> if <paramref name="input" /> is an empty string.
+        /// </summary>
+        /// <param name="guardClause"></param>
+        /// <param name="input"></param>
+        /// <param name="parameterName"></param>
+        /// <param name="message">Optional. Custom error message</param>
+        /// <returns><paramref name="input" /> if the value is not an empty string.</returns>
+        /// <exception cref="ArgumentException"></exception>
+        public static ReadOnlySpan<char> Empty(this IGuardClause guardClause,
+            ReadOnlySpan<char> input,
+            string parameterName,
+            string? message = null)
+        {
+            if (input.Length == 0 || input == string.Empty)
+            {
+                throw new ArgumentException(message ?? $"Required input {parameterName} was empty.", parameterName);
+            }
+            return input;
+        }
+
+        /// <summary>
+        /// Throws an <see cref="ArgumentException" /> if <paramref name="input" /> is an empty or white space string.
+        /// </summary>
+        /// <param name="guardClause"></param>
+        /// <param name="input"></param>
+        /// <param name="parameterName"></param>
+        /// <param name="message">Optional. Custom error message</param>
+        /// <returns><paramref name="input" /> if the value is not an empty or whitespace string.</returns>
+        /// <exception cref="ArgumentException"></exception>
+        public static ReadOnlySpan<char> WhiteSpace(this IGuardClause guardClause,
+            ReadOnlySpan<char> input,
+            string parameterName,
+            string? message = null)
+        {
+            if (MemoryExtensions.IsWhiteSpace(input))
+            {
+                throw new ArgumentException(message ?? $"Required input {parameterName} was empty.", parameterName);
+            }
+
+            return input;
+        }
+#endif
+    }
+}
--- src/GuardClauses/GuardAgainstNullExtensions.cs
+++ src/GuardClauses/GuardAgainstNullExtensions.cs
@@ -79,29 +79,6 @@ namespace Ardalis.GuardClauses
             return input;
         }
 
-#if NET5_0_OR_GREATER
-        /// <summary>
-        /// Throws an <see cref="ArgumentException" /> if <paramref name="input" /> is an empty string.
-        /// </summary>
-        /// <param name="guardClause"></param>
-        /// <param name="input"></param>
-        /// <param name="parameterName"></param>
-        /// <param name="message">Optional. Custom error message</param>
-        /// <returns><paramref name="input" /> if the value is not an empty string.</returns>
-        /// <exception cref="ArgumentException"></exception>
-        public static ReadOnlySpan<char> Empty(this IGuardClause guardClause,
-            ReadOnlySpan<char> input,
-            string parameterName,
-            string? message = null)
-        {
-            if (input.Length == 0 || input == string.Empty)
-            {
-                throw new ArgumentException(message ?? $"Required input {parameterName} was empty.", parameterName);
-            }
-            return input;
-        }
-#endif
-
         /// <summary>
         /// Throws an <see cref="ArgumentNullException" /> if <paramref name="input" /> is null.
         /// Throws an <see cref="ArgumentException" /> if <paramref name="input" /> is an empty guid.
@@ -197,30 +174,6 @@ namespace Ardalis.GuardClauses
 
             return input;
         }
-
-#if NET5_0_OR_GREATER
-        /// <summary>
-        /// Throws an <see cref="ArgumentException" /> if <paramref name="input" /> is an empty or white space string.
-        /// </summary>
-        /// <param name="guardClause"></param>
-        /// <param name="input"></param>
-        /// <param name="parameterName"></param>
-        /// <param name="message">Optional. Custom error message</param>
-        /// <returns><paramref name="input" /> if the value is not an empty or whitespace string.</returns>
-        /// <exception cref="ArgumentException"></exception>
-        public static ReadOnlySpan<char> WhiteSpace(this IGuardClause guardClause,
-            ReadOnlySpan<char> input,
-            string parameterName,
-            string? message = null)
-        {
-            if (MemoryExtensions.IsWhiteSpace(input))
-            {
-                throw new ArgumentException(message ?? $"Required input {parameterName} was empty.", parameterName);
-            }
-
-            return input;
-        }
-#endif
 
         /// <summary>
         /// Throws an <see cref="ArgumentException" /> if <paramref name="input" /> is default for that type.
