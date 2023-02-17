d54c66ae6b8fd4f81716db3a74069b53e078409a
danny-bos-developer <danny.bos.developer@dannybos.nl>
2023-02-17 18:03:17 +0100

    Created async overload for clausules that use a predicate (#228)
    * Created async overload for clausules that use a predicate
    
    * Made GetCustomStruct public (build error)
    
    ---------
    
    Co-authored-by: Danny Bos <danny.bos@tatasteeleurope.com>
    Co-authored-by: Steve Smith <steve@kentsmiths.com>

--- src/GuardClauses/GuardAgainstInvalidFormatExtensions.cs
+++ src/GuardClauses/GuardAgainstInvalidFormatExtensions.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Text.RegularExpressions;
+using System.Threading.Tasks;
 
 namespace Ardalis.GuardClauses
 {
@@ -44,6 +45,27 @@ namespace Ardalis.GuardClauses
         public static T InvalidInput<T>(this IGuardClause guardClause, T input, string parameterName, Func<T, bool> predicate, string? message = null)
         {
             if (!predicate(input))
+            {
+                throw new ArgumentException(message ?? $"Input {parameterName} did not satisfy the options", parameterName);
+            }
+
+            return input;
+        }
+
+        /// <summary>
+        /// Throws an <see cref="ArgumentException" /> if  <paramref name="input"/> doesn't satisfy the <paramref name="predicate"/> function.
+        /// </summary>
+        /// <param name="guardClause"></param>
+        /// <param name="input"></param>
+        /// <param name="parameterName"></param>
+        /// <param name="predicate"></param>
+        /// <param name="message">Optional. Custom error message</param>
+        /// <typeparam name="T"></typeparam>
+        /// <returns></returns>
+        /// <exception cref="ArgumentException"></exception>
+        public static async Task<T> InvalidInputAsync<T>([JetBrainsNotNull] this IGuardClause guardClause, [JetBrainsNotNull] T input, [JetBrainsNotNull][JetBrainsInvokerParameterName] string parameterName, Func<T, Task<bool>> predicate, string? message = null)
+        {
+            if (!await predicate(input))
             {
                 throw new ArgumentException(message ?? $"Input {parameterName} did not satisfy the options", parameterName);
             }
