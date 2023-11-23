dca2acada7900dcb7e3d042878ee5ec3a0df3be7
Simon Nyvall <simon.k.nyvall@proton.me>
2023-11-23 15:02:24 +0100

    Add unit test for expression

--- test/GuardClauses.UnitTests/GuardAgainstExpression.cs
+++ test/GuardClauses.UnitTests/GuardAgainstExpression.cs
@@ -26,60 +26,49 @@ public class GuardAgainstExpression
     [InlineData(10)]
     public void GivenIntegerWhenTheExpressionEvaluatesToTrueDoesNothing(int test)
     {
-        Guard.Against.AgainstExpression((x) => x == 10, test, "Value is not equal to 10");
+        Guard.Against.Expression((x) => x == 10, test, "Value is not equal to 10");
     }
 
     [Theory]
     [InlineData(10)]
     public void GivenIntegerWhenTheExpressionEvaluatesToFalseThrowsException(int test)
     {
-        Assert.Throws<ArgumentException>(() => Guard.Against.AgainstExpression((x) => x == 5, test, "Value is not equal to 10"));
+        Assert.Throws<ArgumentException>(() => Guard.Against.Expression((x) => x == 5, test, "Value is not equal to 10"));
     }
 
     [Theory]
     [InlineData(1.1)]
     public void GivenDoubleWhenTheExpressionEvaluatesToTrueDoesNothing(double test)
     {
-        Guard.Against.AgainstExpression((x) => x == 1.1, test, "Value is not equal to 1.1");
+        Guard.Against.Expression((x) => x == 1.1, test, "Value is not equal to 1.1");
     }
 
     [Theory]
     [InlineData(1.1)]
     public void GivenDoubleWhenTheExpressionEvaluatesToFalseThrowsException(int test)
     {
-        Assert.Throws<ArgumentException>(() => Guard.Against.AgainstExpression((x) => x == 5.0, test, "Value is not equal to 1.1"));
+        Assert.Throws<ArgumentException>(() => Guard.Against.Expression((x) => x == 5.0, test, "Value is not equal to 1.1"));
     }
 
     [Theory]
     [MemberData(nameof(GetCustomStruct))]
     public void GivenCustomStructWhenTheExpressionEvaluatesToTrueDoesNothing(CustomStruct test)
     {
-        Guard.Against.AgainstExpression((x) => x.FieldName == "FieldValue", test, "FieldValue is not matching");
+        Guard.Against.Expression((x) => x.FieldName == "FieldValue", test, "FieldValue is not matching");
     }
 
     [Theory]
     [MemberData(nameof(GetCustomStruct))]
     public void GivenCustomStructWhenTheExpressionEvaluatesToFalseThrowsException(CustomStruct test)
     {
-        Assert.Throws<ArgumentException>(() => Guard.Against.AgainstExpression((x) => x.FieldName == "FailThis", test, "FieldValue is not matching"));
-    }
-
-    [Theory]
-    [InlineData(null, "Value does not fall within the expected range.")]
-    [InlineData("Please provide correct value", "Please provide correct value")]
-    public void ErrorMessageMatchesExpected(string customMessage, string expectedMessage)
-    {
-        var exception = Assert.Throws<ArgumentException>(() => Guard.Against.AgainstExpression(x => x == 1, 2, customMessage));
-        Assert.NotNull(exception);
-        Assert.NotNull(exception.Message);
-        Assert.Equal(expectedMessage, exception.Message);
+        Assert.Throws<ArgumentException>(() => Guard.Against.Expression((x) => x.FieldName == "FailThis", test, "FieldValue is not matching"));
     }
 
     [Fact]
     public void ErrorIncludesParamNameIfProvided()
     {
         string paramName = "testParamName";
-        var exception = Assert.Throws<ArgumentException>(() => Guard.Against.AgainstExpression(x => x == 1, 2, "custom message", paramName));
+        var exception = Assert.Throws<ArgumentException>(() => Guard.Against.Expression(x => x == 1, 2, "custom message", paramName));
         Assert.NotNull(exception);
         Assert.NotNull(exception.Message);
         Assert.Equal(paramName, exception.ParamName);
