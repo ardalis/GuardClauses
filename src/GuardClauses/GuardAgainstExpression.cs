5431b2ca9f8f67401aba09b231f8c998707d04e1
Simon Nyvall <simon.k.nyvall@proton.me>
2023-11-23 14:23:50 +0100

    Update Expression summary

--- src/GuardClauses/GuardAgainstExpression.cs
+++ src/GuardClauses/GuardAgainstExpression.cs
@@ -1,73 +1,60 @@
 ﻿using System;
 using System.Threading.Tasks;
+using System.Runtime.CompilerServices;
 
 namespace Ardalis.GuardClauses;
 
 public static partial class GuardClauseExtensions
 {
     /// <summary>
-    /// Throws an <see cref="ArgumentException" /> if <paramref name="func"/> evaluates to false for given <paramref name="input"/> 
+    /// Validates the <paramref name="input"/> using the specified <paramref name="func"/> and throws an <see cref="ArgumentException"/> if it evaluates to true.
+    /// The <paramref name="func"/> should return true to indicate an invalid or undesirable state of the input.
+    /// If <paramref name="func"/> returns true, an <see cref="ArgumentException"/> is thrown, signifying that the input is invalid.
     /// </summary>
-    /// <typeparam name="T"></typeparam>
-    /// <param name="func"></param>
-    /// <param name="guardClause"></param>
-    /// <param name="input"></param>
-    /// <param name="message"></param>
-    /// <returns><paramref name="input"/> if the <paramref name="func"/> evaluates to true </returns>
-    /// <exception cref="ArgumentException"></exception>
+    /// <typeparam name="T">The type of the input parameter.</typeparam>
+    /// <param name="guardClause">The guard clause instance.</param>
+    /// <param name="func">The function that evaluates the input. It should return true if the input is considered invalid or in a negative state.</param>
+    /// <param name="input">The input to evaluate.</param>
+    /// <param name="message">The message to include in the exception if the input is invalid.</param>
+    /// <param name="parameterName">The name of the parameter to include in the thrown exception, captured automatically from the input expression.</param>
+    /// <returns>The <paramref name="input"/> if the <paramref name="func"/> evaluates to false, indicating a valid state.</returns>
+    /// <exception cref="ArgumentException">Thrown when the validation function returns true, indicating that the input is invalid.</exception>
     public static T Expression<T>(this IGuardClause guardClause,
         Func<T, bool> func,
         T input,
-        string message) where T : struct
+        string message,
+        [CallerArgumentExpression("input")] string? parameterName = null) where T : struct
     {
         if (!func(input))
         {
-            throw new ArgumentException(message);
+            throw new ArgumentException(message, parameterName!);
         }
 
         return input;
     }
 
     /// <summary>
-    /// Throws an <see cref="ArgumentException" /> if <paramref name="func"/> evaluates to false for given <paramref name="input"/> 
+    /// Validates the <paramref name="func"/> asynchronously and throws an <see cref="ArgumentException" /> if it evaluates to false for given <paramref name="input"/>
+    /// The <paramref name="func"/> should return true to indicate an invalid or undesirable state.
+    /// If <paramref name="func"/> returns true, indicating that the input is invalid, an <see cref="ArgumentException"/> is thrown.
     /// </summary>
-    /// <typeparam name="T"></typeparam>
-    /// <param name="func"></param>
-    /// <param name="guardClause"></param>
-    /// <param name="input"></param>
-    /// <param name="message"></param>
+    /// <typeparam name="T">The type of the input parameter.</typeparam>
+    /// <param name="func">The function that evaluates the input. It should return true if the input is considered invalid or in a negative state.</param>
+    /// <param name="guardClause">The guard clause instance.</param>
+    /// <param name="input">The input to evaluate.</param>
+    /// <param name="message">The message to include in the exception if the input is invalid.</param>
+    /// <param name="parameterName">The name of the parameter to include in the thrown exception, captured automatically from the input expression.</param>
     /// <returns><paramref name="input"/> if the <paramref name="func"/> evaluates to true </returns>
-    /// <exception cref="ArgumentException"></exception>
+    /// <exception cref="ArgumentException">Thrown when the validation function returns true, indicating that the input is invalid.</exception>
     public static async Task<T> ExpressionAsync<T>(this IGuardClause guardClause,
         Func<T, Task<bool>> func,
         T input,
-        string message) where T : struct
+        string message,
+        [CallerArgumentExpression("input")] string? parameterName = null) where T : struct
     {
         if (!await func(input))
         {
-            throw new ArgumentException(message);
-        }
-
-        return input;
-    }
-
-    /// <summary>
-    /// Throws an <see cref="ArgumentException" /> if <paramref name="func"/> evaluates to false for given <paramref name="input"/> 
-    /// </summary>
-    /// <typeparam name="T"></typeparam>
-    /// <param name="func"></param>
-    /// <param name="guardClause"></param>
-    /// <param name="input"></param>
-    /// <param name="message"></param>
-    /// <param name="paramName">The name of the parameter that is invalid</param>
-    /// <returns><paramref name="input"/> if the <paramref name="func"/> evaluates to true </returns>
-    /// <exception cref="ArgumentException"></exception>
-    public static T Expression<T>(this IGuardClause guardClause, Func<T, bool> func,
-        T input, string message, string paramName) where T : struct
-    {
-        if (!func(input))
-        {
-            throw new ArgumentException(message, paramName);
+            throw new ArgumentException(message, parameterName!);
         }
 
         return input;
