35396b1a7d43c922b0b63a879df7d8a996a88c12
Ehson Akhmedov <ahmedovehson2004@gmail.com>
2023-12-10 14:56:38 +0500

    Provided new feature, `ChainOfDelegates`

---
+++ src/GuardClauses/GuardAgainstDelegateChainExtensions.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Ardalis.GuardClauses;
+using JetBrains.Annotations;
+
+namespace Ardalis.GuardClauses;
+
+/// <summary>
+/// Chain of Delegates
+/// </summary>
+public static partial class GuardClauseExtensions
+{  
+    /// <summary>
+    /// Throws <seealso cref="NullReferenceException"/>, if delegate's invocation list is empty
+    /// </summary>
+    /// <param name="type"></param>
+    /// <returns></returns>
+    /// <exception cref="ArgumentException"></exception>
+    public static Delegate[] AgainstChains(
+        this IGuardClause _,
+        [ValidatedNotNull][NotNull]Delegate? type)
+    {
+        Guard.Against.Null(type,"Delegate","Type of delegate is null");
+
+        Delegate[] list = type.GetInvocationList();
+        return list;    
+    }
+
+
+}
---
+++ test/GuardClauses.UnitTests/GuardAgainstDelegates.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Ardalis.GuardClauses;
+using Xunit;
+
+namespace GuardClauses.UnitTests;
+public class GuardAgainstDelegates
+{
+
+    [Fact]
+    public void GetChainsOfDelegate()
+    {
+        // Assert
+        Action Chain = Console.WriteLine;
+        Chain += delegate {  };
+        Chain += delegate {  };
+        Chain += delegate {  };
+        Chain += delegate {  };
+        Chain += delegate {  };
+        var list = Guard.Against.AgainstChains(Chain);
+
+        Assert.NotNull(list);
+        Assert.True(list.Length == 6);
+    }
+
+    [Fact]
+    public void ThrowsWhenDelegateIsNull()
+    {
+        Action? Chain = null;
+        Assert.Throws<ArgumentNullException>(() => Guard.Against.AgainstChains(Chain));
+    }
+}
