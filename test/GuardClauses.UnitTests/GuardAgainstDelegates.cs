ab1256a3992220efa7e56edc934cf5eda8494cd6
Ehson <129420305+AkhmedovEhson@users.noreply.github.com>
2023-12-22 11:28:48 +0500

    Support tests

--- test/GuardClauses.UnitTests/GuardAgainstDelegates.cs
+++ test/GuardClauses.UnitTests/GuardAgainstDelegates.cs
@@ -21,9 +21,9 @@ public class GuardAgainstDelegates
         Chain += delegate {  };
         Chain += delegate {  };
         var list = Guard.Against.AgainstChains(Chain);
-
+    
         Assert.NotNull(list);
-        Assert.True(list.Length == 6);
+        Assert.Equal<int>(6,list.Length);
     }
 
     [Fact]
@@ -31,5 +31,24 @@ public class GuardAgainstDelegates
     {
         Action? Chain = null;
         Assert.Throws<ArgumentNullException>(() => Guard.Against.AgainstChains(Chain));
+    }
+
+    [Fact]
+    public void GetFirstChain()
+    {
+        Func<int> Chain = delegate { return 0; };
+        Chain += delegate { return 1; };
+        Chain += delegate { return 2; };
+        Chain += delegate { return 3; };
+        Chain += delegate { return 4; };
+        Chain += delegate { return 5; };
+    
+        var chain = Guard.Against.AgainstChainSelection(Chain, 0);
+        var chain3 = Guard.Against.AgainstChainSelection(Chain, 2);
+        int result = (int)chain.DynamicInvoke();
+        int result3 = (int)chain.DynamicInvoke();
+    
+        Assert.Equal<int>(0,result);
+        Assert.Equal<int>(2,result3);
     }
 }
