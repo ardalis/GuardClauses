cb8418fb0d8755e101c806c9aa1eb419d3e7de36
Ehson Akhmedov <ahmedovehson2004@gmail.com>
2023-12-25 11:30:01 +0500

    Some refactors

--- GuardClauses.FluentValidations/FluentValidationResultExtensions.cs
+++ GuardClauses.FluentValidations/FluentValidationResultExtensions.cs
@@ -4,16 +4,29 @@ using FluentValidation;
 using FluentValidation.Results;
 using GuardClauses.FluentValidations;
 namespace Ardalis.GuardClauses.FluentValidations;
+
+/// <summary>
+/// FluentValidationExtensions
+/// </summary>
 public static class FluentValidationResultExtensions
 {
+    /// <summary>
+    /// Validates and throws
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    /// <param name="guard"></param>
+    /// <param name="input"></param>
+    /// <returns></returns>
+    /// <exception cref="ArgumentException"></exception>
     public static T ValidateOrThrow<T>(
         this IGuardClause guard,
         T? input) 
     {
+
         Guard.Against.Null(input);
 
-        Assembly assembly = Assembly.GetExecutingAssembly();
-
+        Assembly assembly = GuardFluentValidation.GetAssembly() ?? Assembly.GetExecutingAssembly();
+        
         Type? validatorType = AssemblyScanner.FindValidatorsInAssembly(assembly)
             .Select(o => o.ValidatorType)
             .Where(o => o.IsSubclassOf(typeof(AbstractValidator<T>)))
