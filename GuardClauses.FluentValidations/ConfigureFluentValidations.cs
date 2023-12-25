7fee5bb8ed9a7a488501d558590e29c2b4be97e5
Ehson Akhmedov <ahmedovehson2004@gmail.com>
2023-12-25 11:30:56 +0500

    Provide new feature `ConfigureFluentValidations` to configure Assembly

---
+++ GuardClauses.FluentValidations/ConfigureFluentValidations.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GuardClauses.FluentValidations;
+public class GuardFluentValidation
+{
+    private static Assembly? Assembly { get; set; }
+
+    public static void Configure(Assembly assembly)
+    {
+        Assembly = assembly; 
+    }
+
+    public static Assembly? GetAssembly() => Assembly;
+
+}
