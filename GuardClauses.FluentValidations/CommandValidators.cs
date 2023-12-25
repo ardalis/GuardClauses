0e1814b97a0eafd63cfa1701e534b53d65248316
Ehson Akhmedov <ahmedovehson2004@gmail.com>
2023-12-25 09:55:15 +0500

    Feature FluentValidation + GuardClause

---
+++ GuardClauses.FluentValidations/CommandValidators.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FluentValidation;
+
+namespace GuardClauses.FluentValidations;
+public class Command
+{
+    public int Id { get; set; }
+}
+public class CommandWithoutValidator
+{
+    public int Id { get; set; }
+}
+public class CommandValidator : AbstractValidator<Command>
+{
+    public CommandValidator()
+    {
+        RuleFor(o => o.Id).GreaterThan(1);
+    }
+}
---
+++ GuardClauses.FluentValidations/FluentValidationResultExtensions.cs
@@ -0,0 +1,35 @@
+﻿
+using System.Reflection;
+using FluentValidation;
+using FluentValidation.Results;
+using GuardClauses.FluentValidations;
+namespace Ardalis.GuardClauses.FluentValidations;
+public static class FluentValidationResultExtensions
+{
+    public static T ValidateOrThrow<T>(
+        this IGuardClause guard,
+        T? input) 
+    {
+        Guard.Against.Null(input);
+
+        Assembly assembly = Assembly.GetExecutingAssembly();
+
+        Type? validatorType = AssemblyScanner.FindValidatorsInAssembly(assembly)
+            .Select(o => o.ValidatorType)
+            .Where(o => o.IsSubclassOf(typeof(AbstractValidator<T>)))
+            .FirstOrDefault();
+
+        Guard.Against.Null(validatorType);    
+
+        IValidator? validator = (IValidator?)Activator.CreateInstance(validatorType);
+        var context = new ValidationContext<T>(input);
+        var response = validator!.Validate(context);
+
+        if (!response.IsValid)
+        {
+            throw new ArgumentException($"Input '{input.GetType().Name}' is not validated");
+        }
+        return input;
+    }
+
+}
