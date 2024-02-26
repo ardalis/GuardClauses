78524aca2afe821f75d74c84f9bd90eb1b2ed315
Rudi Larno <Rudi.Larno@gmail.com>
2024-02-26 16:58:36 +0100

    Update GuardAgainstStringLengthExtensions.cs
    Namespace and Maxmimum typo

--- src/GuardClauses/GuardAgainstStringLengthExtensions.cs
+++ src/GuardClauses/GuardAgainstStringLengthExtensions.cs
@@ -2,7 +2,7 @@
 using System.Runtime.CompilerServices;
 using Ardalis.GuardClauses;
 
-namespace GuardClauses;
+namespace Ardalis.GuardClauses;
 
 /// <summary>
 /// The class containing extension methods for <see cref="IGuardClause"/> 
@@ -53,7 +53,7 @@ public static partial class GuardClauseExtensions
         Guard.Against.NegativeOrZero(maxLength, nameof(maxLength));
         if (input.Length > maxLength)
         {
-            throw new ArgumentException(message ?? $"Input {parameterName} with length {input.Length} is too long. Maxmimum length is {maxLength}.", parameterName);
+            throw new ArgumentException(message ?? $"Input {parameterName} with length {input.Length} is too long. Maximum length is {maxLength}.", parameterName);
         }
         return input;
     }
